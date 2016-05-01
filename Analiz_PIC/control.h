d46d4e0a555051840cf8c761b9c89b2131e72aaf
Mauricio Jost <mauricio.jost@gmail.com>
2016-05-01 12:54:27 +0200

    Remove unneeded files

--- Analiz_PIC/control.h
+++
@@ -1,8 +0,0 @@
-void inicializacion(){
-	lcd_iniciar();
-	set_tris_b(0xFF); // original
-	lcd_presentacion();
-	disable_interrupts(INT_EXT); // Configuración de las interrupciones.
-	disable_interrupts(INT_RB); // Configuración de las interrupciones.
-	enable_interrupts(GLOBAL);
-}
